e77efa3c08e9db2219e9b500c6a9037e35cd969d
snso <snsopush@sina.com>
2017-06-15 18:04:50 +0800

    单向链表

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include<iostream>
 
 #include "./list/SeqList.h" 
+#include "./list/LinkList.h" 
 
 using namespace std;
 
@@ -43,6 +44,13 @@ class Test
 
 Test* Test::test = NULL;
 
+
+struct Value
+{
+    LinkListNode header;
+    int v;
+};
+
 int main()
 {
     
@@ -55,6 +63,7 @@ int main()
     Complex c =  c1+c2;
     
     /**线性列表测试*/
+    /*
     SeqList* list = SeqList_Create(5);
     
     int i = 0;
@@ -89,7 +98,44 @@ int main()
     }
     
     SeqList_Destroy(list);
+    */
+    //链表存储测试 
+    int i = 0;
+    LinkList* list = LinkList_Create();
     
+    struct Value v1;
+    struct Value v2;
+    struct Value v3;
+    struct Value v4;
+    struct Value v5;
+    
+    v1.v = 1;
+    v2.v = 2;
+    v3.v = 3;
+    v4.v = 4;
+    v5.v = 5;
+    
+    LinkList_Insert(list, (LinkListNode*)&v1, LinkList_Length(list));
+    LinkList_Insert(list, (LinkListNode*)&v2, LinkList_Length(list));
+    LinkList_Insert(list, (LinkListNode*)&v3, LinkList_Length(list));
+    LinkList_Insert(list, (LinkListNode*)&v4, LinkList_Length(list));
+    LinkList_Insert(list, (LinkListNode*)&v5, LinkList_Length(list));
+    
+    for(i=0; i<LinkList_Length(list); i++)
+    {
+        struct Value* pv = (struct Value*)LinkList_Get(list, i);
+        
+        printf("%d\n", pv->v);
+    }
+    
+    while( LinkList_Length(list) > 0 )
+    {
+        struct Value* pv = (struct Value*)LinkList_Delete(list, 0);
+        
+        printf("%d\n", pv->v);
+    }
+    
+    LinkList_Destroy(list);
 
           
      system("PAUSE");
